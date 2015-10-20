92898ba69e805e58380b89e1936cdee656bdca14
Michael G. Ströck michael@stroeck.com <michael@stroeck.com>
2015-10-20 21:56:00 +0200

    Show teams in Spark dropdown.

--- resources/views/vendor/spark/nav/spark/dropdown.blade.php
+++ resources/views/vendor/spark/nav/spark/dropdown.blade.php
@@ -13,28 +13,43 @@
         </a>
     </li>
 
-    <!-- Team Settings / List -->
-    @if (Spark::usingTeams())
-        <!-- Team Navigation Options -->
-        <!-- Team Settings -->
-        <li v-if="user.current_team_id">
-            <a href="/settings/teams/@{{ user.current_team_id }}">
-                <i class="fa fa-btn fa-fw fa-cog"></i>Team Settings
-            </a>
-        </li>
+		<!-- Team Settings / List -->
+		@if (Spark::usingTeams())
+			<!-- Team Navigation Options -->
+			<!-- Team Settings -->
+			@if (Auth::user()->current_team_id)
+				<li>
+					<a href="/settings/teams/{{ Auth::user()->current_team_id }}">
+						<i class="fa fa-btn fa-fw fa-cog"></i>Team Settings
+					</a>
+				</li>
+			@endif
 
-        <li class="divider"></li>
+			<li class="divider"></li>
 
-        <li class="dropdown-header">Teams</li>
+			<li class="dropdown-header">Teams</li>
 
-        <!-- Create New Team -->
-        <li>
-            <a href="/settings?tab=teams">
-                <i class="fa fa-btn fa-fw fa-plus"></i>Create New Team
-            </a>
-        </li>
+			<!-- Create New Team -->
+			<li>
+				<a href="/settings?tab=teams">
+					<i class="fa fa-btn fa-fw fa-plus"></i>Create New Team
+				</a>
+			</li>
 
-    @endif
+			<!-- Team Listing -->
+			@foreach (Auth::user()->teams as $team)
+				<li>
+					<a href="/settings/teams/switch/{{ $team->id }}">
+						@if ($team->id === Auth::user()->current_team_id)
+							<i class="fa fa-btn fa-fw fa-check text-success"></i>{{ $team->name }}
+						@else
+							<i class="fa fa-btn fa-fw"></i>{{ $team->name }}
+						@endif
+					</a>
+				</li>
+			@endforeach
+		@endif
+
 
     <!-- Logout -->
     <li class="divider"></li>
