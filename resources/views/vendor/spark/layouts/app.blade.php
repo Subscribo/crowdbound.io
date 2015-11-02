00e125d37911308813af35801cbabbcb1ec73519
Michael G. Ströck michael@stroeck.com <michael@stroeck.com>
2015-11-02 17:41:06 +0100

    Some edits to Spark blade templates.

--- resources/views/vendor/spark/layouts/app.blade.php
+++ resources/views/vendor/spark/layouts/app.blade.php
@@ -22,6 +22,7 @@
     @include('spark::common.footer')
 
     <!-- JavaScript Application -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js"></script>
     <script src="{{ elixir('assets/js/laravel-spark-app.js') }}"></script>
 </body>
 </html>
